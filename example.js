ceaf5d5d55ef1bdfaaa3ee9cd4062d60981b0e02
derxe <42929423+derxe@users.noreply.github.com>
2018-10-05 18:35:02 +0200

    Change url to redacted.ch

--- example.js
+++ example.js
@@ -2,7 +2,7 @@
 
 var WhatCD = require("./lib/whatcd");
 
-var client = new WhatCD("https://what.cd", "username", "password");
+var client = new WhatCD("https://redacted.ch", "username", "password");
 
 client.index(function(err, data) {
   if (err) {
